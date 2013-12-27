8ebc15004834294a3e8cf4afc45c0559e31ca9a2
Johnbastian <j.emilianus-12@student.lboro.ac.uk>
2013-12-27 22:53:43 +0000

    Basic site setup
    Basic pages created
    AJAX implemented for Parent-Child system

---
+++ JS/indexScript.js
@@ -0,0 +1,31 @@
+/*When index.html is loaded and ready */
+$(document).ready(function() {
+	
+	/* Initial Load - load homepage */ /* Need to validate for redirection to login page*/
+	$('#ContentDiv').load('home.html');	
+	
+	/* Handels on-click of menu buttons - block coding */
+	$('#MenuBar a').click(function(){
+		
+		/* Getting the linked page */
+		var toLoad = $(this).attr('href');		
+		
+		/* Hiding content and calling loadContent function - creates animation*/
+		$('#ContentDiv').hide('slow', loadContent);
+		
+		/* Injects content to ContentDiv div tag */
+		function loadContent(){
+			$('#ContentDiv').load(toLoad, '', showNewContent);
+		}
+		
+		/* Shows content div */
+		function showNewContent() {
+			$('#ContentDiv').show('normal');
+		}
+		
+		/* In order to stop the browser actually navigating to the page, false is returned */
+		return false;
+		
+	});
+	
+});
