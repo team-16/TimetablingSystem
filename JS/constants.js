dfc8589b3183dc7b0e8c373c7144c57bfce4cefa
Johnbastian <j.emilianus-12@student.lboro.ac.uk>
2013-12-28 01:25:24 +0000

    Added Constants file
    Added a constants.js file, where all assumed (hardly changing) elements
    are stored in the form of arrays.

---
+++ JS/constants.js
@@ -0,0 +1,14 @@
+var parksArray = ["East", "Central", "West"];
+
+var sessionTypesArray = ["Lecture", "Tutorial", "Lab"];
+
+var daysArray = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+
+var shortDaysArray = ["Mon", "Tue", "Wed", "Thu", "Fri"];
+
+var periodsArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+var periodTimesArray = ["09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00"];
+
+var statusArray = ["Pending", "Allocated", "Rejected"];
+
