0d355ec8ac44ccba384ae7441606d9636faddf67
Johnbastian <j.emilianus-12@student.lboro.ac.uk>
2014-02-12 15:54:32 +0000

    Some files added

--- JS/dataManager.js
+++ JS/dataManager.js
@@ -1,12 +1,11 @@
 function getModules() {
 	
-	$.get("view.php", {"rT":"full", "sqlStatement":sql}, function(results){
+	$.getJSON("init.php echo getModules('CO')", {"rT":"full", "sqlStatement":sql}, function(results){
 			
 			//Makes results global so that other functions can access them
-			rankedArray = results;
+			//rankedArray = results;
 			
-			//Calls populate table to use data to create results table
-			populateTable();
+			alert(JSON.stringify(results));
 			
 		}, 'json');
 	
