1213e7473e2d23174eee4398d2b543c452736ba6
Niall <niall@live.co.uk>
2014-02-12 12:31:23 +0000

    Set default password for departments as "password"

--- test.php
+++ test.php
@@ -9,12 +9,31 @@ include('PHP/init.php');
 echo("// Testing Login Script...  ");
 
 //change this to test login. this should fail if you change the password.
-login("BS", "lel");
+login("BS", "password");
 
 // change password
-changePassword("BS", "lel");
+changePassword("BS", "password");
+changePassword("CG", "password");
+changePassword("CM", "password");
+changePassword("CO", "password");
+changePassword("CV", "password");
+changePassword("DS", "password");
+changePassword("EA", "password");
+changePassword("EC", "password");
+changePassword("EL", "password");
+changePassword("EU", "password");
+changePassword("GY", "password");
+changePassword("IS", "password");
+changePassword("MA", "password");
+changePassword("MM", "password");
+changePassword("MP", "password");
+changePassword("PH", "password");
+changePassword("PS", "password");
+changePassword("SA", "password");
+changePassword("SS", "password");
+changePassword("TT", "password");
 
 // check new password works
-login("BS", "lel");
+login("BS", "password");
 
 ?>
