2bd46bfd9c944daf58346b0c9d3c05e7fd0cd3e1
Niall <niall@live.co.uk>
2014-02-12 14:30:36 +0000

    Added a backup function for duplicating a request to a new round

--- PHP/request.php
+++ PHP/request.php
@@ -87,8 +87,7 @@ function insertRequest($moduleCode, $priority, $day, $startPeriod, $endPeriod,
 }
 
 
-function deleteRequest($id)
-{
+function deleteRequest($id){
 	global $DB;
 	
 	if($DB->query("SELECT request WHERE id = :id", array(':id' => $id))){
@@ -100,5 +99,24 @@ function deleteRequest($id)
 	}
 	
 }
+
+/*function copyRequestToRound($id, $newRound){
+	
+	global $DB;
+	
+	if($DB->query("SELECT request WHERE id = :id", array(':id' => $id))){
+		$DB->query("INSERT INTO room (id, moduleCode, priority, day,
+		startPeriod, endPeriod, weeks, noOfStudents, parkPreference, traditional,
+		sessionType, noOfRooms, roomCode, otherRequirements, roundID, status)
+		SELECT :id, moduleCode, priority, day,
+		startPeriod, endPeriod, weeks, noOfStudents, parkPreference, traditional,
+		sessionType, noOfRooms, roomCode, otherRequirements, :newround, status
+		FROM room WHERE id=:id", array(':id' => $id, ':newround' => $newRound));
+	}
+	
+	else{
+		return false;
+	}
+} */
 
 ?>
