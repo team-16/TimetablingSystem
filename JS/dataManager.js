487152a02ffe7ceb8e977d353180a6ad2f966672
Johnbastian <j.emilianus-12@student.lboro.ac.uk>
2014-02-12 15:16:35 +0000

    Testing get modules

--- JS/dataManager.js
+++ JS/dataManager.js
@@ -1,6 +1,14 @@
 function getModules() {
 	
-	
+	$.get("view.php", {"rT":"full", "sqlStatement":sql}, function(results){
+			
+			//Makes results global so that other functions can access them
+			rankedArray = results;
+			
+			//Calls populate table to use data to create results table
+			populateTable();
+			
+		}, 'json');
 	
 	
 }
