250fd743b75b414dc2f12ae797e7276627b91109
bennyboy23 <b.j.e.evans-12@student.lboro.ac.uk>
2014-02-13 16:56:55 +0000

    Update add.js
    added a ranged slider to the add page

--- JSViews/add.js
+++ JSViews/add.js
@@ -136,3 +136,16 @@ function onKeyUpCheck(){
     plsNoZero();    
     maxValue();
 }
+$(function rangedSlider() {
+    $( "#slider-range" ).slider({
+        range: true,
+        min: 1,
+        max: 10,
+        values: [ 1, 10 ],
+        slide: function( event, ui ) {
+            $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+        }
+        });
+        $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+        " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+});
